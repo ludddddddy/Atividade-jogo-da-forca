18c5f616211e4e1f41a074479d33c32128b2ab8e
Ludmylla Dias <68711217+ludddddddy@users.noreply.github.com>
2025-09-27 15:21:19 +0000

    commit1

---
+++ jogo-da-forca/src/app/layout.js
@@ -0,0 +1,28 @@
+
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Jogo da Forca",
+  description: "Feito em Next.js + Tailwind",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-BR">
+      <body className="bg-gray-100 text-gray-800 flex items-center justify-center min-h-screen">
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ jogo-da-forca/src/app/page.js
@@ -0,0 +1,15 @@
+
+import Hangman from "@/component/Hangman";
+
+export default function Home() {
+  return (
+    <main className="w-full max-w-2xl bg-white shadow-lg rounded-2xl p-6">
+      <h1 className="text-3xl font-bold text-center mb-6 text-blue-700">
+        🎯 Jogo da Forca
+      </h1>
+      <Hangman />
+    </main>
+  );
+}
+
+
